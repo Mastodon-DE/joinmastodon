bf1f9f193b7c10cc12fbe5b6d5117046ea0df31f
Jesse Wierzbinski <contact@cpluspatch.com>
2023-08-26 17:04:15 -1000

    New feature section

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,27 @@ const people = [
 	},
 	// More people...
 ];
+
+const features = [
+	{
+		name: "Powerful moderation tools",
+		description:
+			"Block any keyword from appearing in your timelines with filters",
+		icon: "tabler:hammer",
+	},
+	{
+		name: "Make your own feeds",
+		description:
+			"You alone should decide what should show up in your feeds. Mastodon.de lets you curate your own feeds without a predetermined algorithm",
+		icon: "tabler:timeline",
+	},
+	{
+		name: "Account portability",
+		description:
+			"If you want to change Fediverse servers, you can easily switch and keep all your followers from mastodon.de!",
+		icon: "tabler:road",
+	},
+];
 </script>
 
 <template>
@@ -102,8 +123,8 @@ const people = [
 		</div>
 	</div>
 
-	<div class="mx-auto max-w-7xl px-6 pb-24 sm:pb-32">
-		<div class="py-32">
+	<div class="mx-auto max-w-7xl pb-24 sm:pb-32">
+		<div class="py-32 px-6">
 			<div class="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
 				<div
 					class="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
@@ -173,7 +194,7 @@ const people = [
 			</div>
 		</div>
 
-		<div class="py-24 sm:py-32">
+		<div class="py-24 sm:py-32 px-6">
 			<div class="mx-auto max-w-7xl px-6 lg:px-8">
 				<div class="mx-auto max-w-2xl sm:text-center">
 					<h2
@@ -211,6 +232,78 @@ const people = [
 						</div>
 					</li>
 				</ul>
+			</div>
+		</div>
+
+		<div
+			class="relative isolate overflow-hidden bg-dark-950 px-6 py-20 sm:rounded-3xl sm:px-10 sm:py-24 lg:py-24 xl:px-24">
+			<div
+				class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-y-0">
+				<div class="lg:row-start-2 lg:max-w-md">
+					<h2
+						class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+						Federated, open network
+					</h2>
+					<p class="mt-6 text-lg leading-8 text-gray-300">
+						Mastodon.de is connected to a gigantic federated network
+						of millions of users, the Fediverse.<br />
+						It's like email, where everybody has an address like
+						name@site.com !
+					</p>
+				</div>
+				<img
+					src="/images/screenshots/desktop1.webp"
+					alt="Product screenshot"
+					class="relative -z-20 min-w-full max-w-xl rounded-xl shadow-xl ring-1 ring-white/10 lg:row-span-4 lg:w-[64rem] lg:max-w-none"
+					width="2432"
+					height="1442" />
+				<div
+					class="max-w-xl lg:row-start-3 lg:mt-10 lg:max-w-md lg:border-t lg:border-white/10 lg:pt-10">
+					<dl
+						class="max-w-xl space-y-8 text-base leading-7 text-gray-300 lg:max-w-none">
+						<div
+							v-for="feature in features"
+							:key="feature.name"
+							class="relative">
+							<dt
+								class="ml-9 inline-block font-semibold text-white">
+								<Icon
+									:name="feature.icon"
+									class="absolute left-1 top-1 h-5 w-5 text-red-500"
+									aria-hidden="true" />
+								{{ feature.name }}
+							</dt>
+							{{ " " }}
+							<dd class="inline">{{ feature.description }}</dd>
+						</div>
+					</dl>
+				</div>
+			</div>
+			<div
+				class="pointer-events-none absolute left-12 top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-3xl lg:bottom-[-12rem] lg:top-auto lg:translate-y-0 lg:transform-gpu"
+				aria-hidden="true">
+				<div
+					class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-rose-400 to-red-700 opacity-25"
+					style="
+						clip-path: polygon(
+							74.1% 44.1%,
+							100% 61.6%,
+							97.5% 26.9%,
+							85.5% 0.1%,
+							80.7% 2%,
+							72.5% 32.5%,
+							60.2% 62.4%,
+							52.4% 68.1%,
+							47.5% 58.3%,
+							45.2% 34.5%,
+							27.5% 76.7%,
+							0.1% 64.9%,
+							17.9% 100%,
+							27.6% 76.8%,
+							76.1% 97.7%,
+							74.1% 44.1%
+						);
+					" />
 			</div>
 		</div>
 	</div>
